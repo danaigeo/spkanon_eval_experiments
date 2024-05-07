78d724f55c865e0a08cbd701a2bb6f94559a1673
Danai Georgiou <georgiou@virt-3326.kl.dfki.de>
2024-05-07 17:46:22 +0200

    experimentation scripts

---
+++ experimentation_with_enroll.py
@@ -0,0 +1,278 @@
+import os
+import pickle
+import json
+
+import random
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.metrics import roc_curve, RocCurveDisplay
+import numpy as np
+import plda
+from omegaconf import OmegaConf
+from matplotlib import pyplot as plt
+import torch
+from argparse import ArgumentParser
+
+from spkanon_eval.setup_module import setup
+from spkanon_eval.datamodules.dataset import load_audio
+from spkanon_eval.evaluation.asv.asv_utils import analyse_results, compute_eer, compute_llrs
+from spkanon_eval.evaluation.asv.spkid_plda import ASV
+from spkanon_eval.evaluation.analysis import get_characteristics
+
+def load_utterances(filename):
+    """
+    Function to load utterances from the original file
+    
+    Parameters:
+    filename: The path to the data file containing 
+    Returns:
+    utterances_by_speaker: dictionary where keys are speaker IDs and values are lists of utterances
+
+    """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    utterances_by_speaker = {}
+    for line in lines:
+        data = json.loads(line.strip())
+        speaker_id = data['speaker_id']
+        utterance = data['path']
+        if speaker_id not in utterances_by_speaker:
+            utterances_by_speaker[speaker_id] = []
+        utterances_by_speaker[speaker_id].append(data)
+    return utterances_by_speaker
+
+# Function to filter out speakers
+def filter_speakers_with_few_utterances(scenario, seed, all_utterances, min_utterances_per_speaker, file_name, stage):
+    """
+    Filter out speakers that don't have enough utterances for the experiment
+
+    Parameters:
+    all_utterances (dict): A dictionary where each key is a speaker_id and each value is a list of utterances.
+    min_utterances_per_speaker (int): Minimum number of utterances allowed per speaker.
+
+    Returns:
+    filtered_trials: The filtered trial speaker_ids .
+    filtered_enrolls: The filtered enrolled speaker_ids
+    """
+    selected_speakers = [] 
+    for speaker_id, utterances in all_utterances.items():
+        # Check if the speaker has enough utterances
+        if len(utterances) >= min_utterances_per_speaker:
+            selected_speakers.append(speaker_id)
+            
+    ids = []
+    with open(file_name, 'r') as file:
+        for line in file:
+            obj = json.loads(line.strip())
+            if obj["speaker_id"] in selected_speakers:
+                ids.append(obj)
+                    
+    filtered_file = f"filtered_{scenario}_{stage}{seed}.txt"
+    with open(filtered_file, 'w') as temp_file:
+        for entry in ids:
+            temp_file.write(json.dumps(entry) + "\n")
+
+    return filtered_file   
+
+def select_random_utterances(all_utterances, num_utterances_per_speaker):
+    """
+    Selects a random set of utterances for each speaker from a dictionary of utterances,
+    excluding any speaker with fewer utterances than the specified number per speaker.
+
+    Parameters:
+    all_utterances (dict): A dictionary where each key is a speaker_id and each value is a list of utterances.
+    num_utterances_per_speaker (int): Number of random utterances to select per speaker.
+
+    Returns:
+    list: A list containing the selected random utterances.
+    """
+    selected_utterances = []
+    for speaker_id, utterances in all_utterances.items():
+        selected = random.sample(utterances, min(len(utterances), num_utterances_per_speaker))
+        for utterance in selected:
+            selected_utterances.append(utterance)
+    return selected_utterances
+
+def filter_out_trial_utterances(f_enrolls, original_eval, root_folder, trial_folder, seed, scenario, stage):
+    
+    with open(f_enrolls, "r") as enrolls_file:
+        used_utterances = set()
+        used_speaker_ids = set()
+        for line in enrolls_file:
+            utterance = json.loads(line.strip())
+            used_utterances.add(utterance["text"])
+            used_speaker_ids.add(utterance["speaker_id"])
+
+
+    # Initialize a dictionary to store utterances per speaker
+    utterances_per_speaker = {}
+    
+    # Read the original eval file, filter out speaker_ids that were excluded from the enrolls
+    with open(original_eval, "r") as trial_file:
+        for line in trial_file:
+            utterance = json.loads(line.strip())
+            speaker_id = utterance["speaker_id"]
+            if speaker_id in used_speaker_ids:
+                if speaker_id not in utterances_per_speaker:
+                    utterances_per_speaker[speaker_id] = []
+                utterances_per_speaker[speaker_id].append(utterance)
+                
+    # Select 1 random trial per speaker, ensuring they are not in used_utterances
+    selected_utterances = []
+    for speaker_id, utterances in utterances_per_speaker.items():
+        available_utterances = [utt for utt in utterances if utt["text"] not in used_utterances]
+        if available_utterances:
+            selected_utterance = random.choice(available_utterances)
+            selected_utterances.append(selected_utterance)
+            
+    # Write selected trial utterances to a new file
+    f_trials =f"s_filtered_trial_{scenario}_{stage}{seed}.txt"
+    with open(f_trials, "w") as output_file:
+        if root_folder is not None:
+            for trial_utt in selected_utterances:
+                trial_utt["path"] = trial_utt["path"].replace(root_folder, trial_folder)
+                output_file.write(json.dumps(trial_utt) + '\n')
+    return f_trials
+
+def sample_and_filter_speakers(args, write_file, max_utter, filtered_utterances_by_speaker, increment_enroll, iterations_per_group, f_eval, root_folder, seed = None):
+    eval_folder = args.config
+    scenario = args.scen
+
+    asv_folder = os.path.join("spkanon_eval/logs/stargan/train/default-360/1711973011/eval/asv-plda", scenario, "train")
+    trial_folder = os.path.join(eval_folder, "results", "eval")
+    enroll_folder =  os.path.join(eval_folder, "results", "eval_enrolls")
+    sample_rate = 16000
+
+    lda_model = pickle.load(open(os.path.join(asv_folder, "models", "lda.pkl"), "rb"))
+    plda_model = pickle.load(open(os.path.join(asv_folder, "models", "plda.pkl"), "rb"))
+
+    spkemb_config = OmegaConf.load("spkanon_eval/config/components/spkid/xvector.yaml")
+    spkemb_config.spkid.emb_model_ckpt = None
+    spkemb_model = setup(spkemb_config, "cpu")["spkid"]
+
+    spkemb_config.spkid.batch_size = 24
+    asv_config = OmegaConf.create(
+        {
+            "scenario": scenario,
+            "spkid": spkemb_config.spkid,
+            "lda_ckpt": os.path.join(asv_folder, "models", "lda.pkl"),
+            "plda_ckpt": os.path.join(asv_folder, "models", "plda.pkl"),
+            "data": {
+                "config": {
+                    "batch_size": 2,
+                    "num_workers": 10,
+                    "sample_rate": 16000,
+                }
+            }
+        }
+    )
+    asv = ASV(asv_config, "cpu", torch.nn.Linear(256, 256))
+
+    num_utterances_per_speaker = args.start
+    # Optional: Set a seed for reproducibility
+
+    if seed is not None:
+        random.seed(seed)
+        
+    for increase in range(1, 90):
+        with open(write_file, "a") as f:
+                f.write(f"\n")
+
+        num_utterances_per_speaker = num_utterances_per_speaker + increment_enroll
+        for iteration in range(iterations_per_group):
+
+            selected_utterances = select_random_utterances(filtered_utterances_by_speaker, num_utterances_per_speaker)
+            # Save the selected utterances to a new file for each resample
+            selected_filtered_enrolls = f"s_filtered_enrolls{seed}{scenario}.txt"
+            with open(selected_filtered_enrolls, 'w') as file:
+                for utterance in selected_utterances:
+                    file.write(json.dumps(utterance) + '\n')
+
+            # Fitler out 1 random trial utterances that is not included enrolls
+            selected_filtered_trials = filter_out_trial_utterances(selected_filtered_enrolls, f_eval, root_folder, trial_folder, seed, scenario, "trial")
+
+            # compute SpkId vectors of all utts and map them to PLDA space
+            vecs, labels = dict(), dict()
+            for name, f in zip(["trials", "enrolls"], [selected_filtered_trials, selected_filtered_enrolls]):
+                vecs[name], labels[name] = asv.compute_spkid_vecs(f)
+                vecs[name] -= np.mean(vecs[name], axis=0)
+                if asv.lda_model is not None:
+                    vecs[name] = asv.lda_model.transform(vecs[name])
+                vecs[name] = asv.plda_model.model.transform(
+                    vecs[name], from_space="D", to_space="U_model"
+                )
+                    
+            # compute LLRs of all pairs of trial and enrollment utterances
+            llrs, pairs = compute_llrs(asv.plda_model, vecs, 5000)
+            del vecs
+            
+            # map utt indices to speaker indices
+            pairs[:, 0] = labels["trials"][pairs[:, 0]]
+            pairs[:, 1] = labels["enrolls"][pairs[:, 1]]
+            print("No. of trials speakers:", len(labels["trials"]))
+            print("No. of enroll speakers:", len(labels["enrolls"]))
+
+            
+            print("Averaging LLRs across speakers")
+            print(f"No. of speaker pairs: {pairs.shape[0]}")
+            
+            # avg. LLRs across speakers and dump them to the experiment folder
+            unique_pairs, inverse = np.unique(pairs, axis=0, return_inverse=True)
+            llr_avgs = np.bincount(inverse, weights=llrs) / np.bincount(inverse)
+            print("There are", len(unique_pairs), "unique pairs of trial-enroll embeddings")
+
+            trials = unique_pairs[:, 0]
+            enrolls =  unique_pairs[:, 1]  
+
+            fpr, tpr, thresholds, key = compute_eer(unique_pairs[:, 0], unique_pairs[:, 1], llr_avgs)
+            eer = (fpr[key] + (1 - tpr[key])) / 2
+            print(f"Iteration: {iteration}, Num_enrolls {num_utterances_per_speaker}, {llr_avgs.shape}, {thresholds[key]}, {eer}")
+            print("Same-speaker accuracy:", np.sum(llrs[pairs[:, 0] == pairs[:, 1]] > thresholds[key]) / len(pairs))
+            print("Different-speaker accuracy:", np.sum(llrs[pairs[:, 0] != pairs[:, 1]] < thresholds[key]) / len(pairs))
+            
+            with open(write_file, "a") as f:
+                if scenario == "ignorant":
+                    f.write(f"{seed} {args.file} {num_utterances_per_speaker} {len(unique_pairs)} {pairs.shape[0]} {thresholds[key]} {eer}\n")
+                else:
+                    f.write(f" {thresholds[key]} {eer}\n")
+
+
+
+def main(args):
+    """
+    1. Select the .txt with the experiments.
+    2. Plot the EERs of the ignorant and lazy-informed scenario.
+    """
+    eval_folder = args.config
+    scenario = args.scen
+    seed = args.seed # Seed for random number generation for reproducibility
+    f_eval = os.path.join(eval_folder, "data", "eval.txt")
+    if scenario == "ignorant":
+        f_enrolls = os.path.join(eval_folder, "data", "eval_enrolls.txt")
+    else:
+        f_enrolls = os.path.join(eval_folder, "data", "anon_eval_enrolls.txt")
+
+    max_utter = args.max_utter # Maximum number of enrollment utterances
+    utterances_by_speaker = load_utterances(f_enrolls) # Save the utterances per speaker in a dictionary
+    filtered_enrolls = filter_speakers_with_few_utterances(scenario, seed, utterances_by_speaker, max_utter, f_enrolls, "enroll") # Filter out the speakers that have enough utterances to experiment with
+    filtered_utterances_by_speaker = load_utterances(filtered_enrolls) # Save the utterances of the filtered out speaker_ids in a new dictionar
+    increment_enroll = args.increment # Increment number of enrollment utterances for each step
+    iterations_per_group = 5 # Number of times you want to repeat the sampling process for each group size
+    root_folder = "/ds/audio"
+    write_file =args.eval_file
+
+    sample_and_filter_speakers(args, write_file, max_utter, filtered_utterances_by_speaker, increment_enroll, iterations_per_group, f_eval, root_folder, seed)
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--config", type=str, help="Path to eval folder")
+    parser.add_argument("--eval_file", type=str, help="Path to eval file", default= "spkanon_eval/results_stargan_enroll/ls-train-other-500/stargan_num_enrolls#5-lz.txt")
+    parser.add_argument("--scen", type=str, help="Which scenario", default ="ignorant")
+    parser.add_argument("--seed", type=int, help="Seed index", default=52)
+    parser.add_argument("--file", type=int, help="File", default="1")
+    parser.add_argument("--increment", type=int, help="Increment by", default=5)
+    parser.add_argument("--start", type=int, help="Start num utterance", default=80)
+    parser.add_argument("--max_utter", type=int, help="End num utterance", default=120)
+    
+    args = parser.parse_args()
+    main(args)
---
+++ experimentation_with_speakers.py
@@ -0,0 +1,177 @@
+import os
+import pickle
+import json
+
+import random
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.metrics import roc_curve, RocCurveDisplay
+import numpy as np
+import plda
+from omegaconf import OmegaConf
+from matplotlib import pyplot as plt
+import torch
+from argparse import ArgumentParser
+
+from spkanon_eval.setup_module import setup
+from spkanon_eval.datamodules.dataset import load_audio
+from spkanon_eval.evaluation.asv.asv_utils import analyse_results, compute_eer, compute_llrs
+from spkanon_eval.evaluation.asv.spkid_plda import ASV
+from spkanon_eval.evaluation.analysis import get_characteristics
+
+def sample_and_filter_speakers(args,write_file, start_speakers, end_speakers, increment_speakers, iterations_per_group, seed=None):
+    eval_folder =args.config
+    scenario = args.scen
+
+    asv_folder = os.path.join("spkanon_eval/logs/stargan/train/default-360/1711973011/eval/asv-plda", scenario, "train")
+    trial_folder = os.path.join(eval_folder, "results", "eval")
+    enroll_folder =  os.path.join(eval_folder, "results", "eval_enrolls")
+    sample_rate = 16000
+
+    lda_model = pickle.load(open(os.path.join(asv_folder, "models", "lda.pkl"), "rb"))
+    plda_model = pickle.load(open(os.path.join(asv_folder, "models", "plda.pkl"), "rb"))
+
+    spkemb_config = OmegaConf.load("spkanon_eval/config/components/spkid/xvector.yaml")
+    spkemb_config.spkid.emb_model_ckpt = None
+    spkemb_model = setup(spkemb_config, "cpu")["spkid"]
+
+    spkemb_config.spkid.batch_size = 24
+    asv_config = OmegaConf.create(
+        {
+            "scenario": scenario,
+            "spkid": spkemb_config.spkid,
+            "lda_ckpt": os.path.join(asv_folder, "models", "lda.pkl"),
+            "plda_ckpt": os.path.join(asv_folder, "models", "plda.pkl"),
+            "data": {
+                "config": {
+                    "batch_size": 2,
+                    "num_workers": 10,
+                    "sample_rate": 16000,
+                }
+            }
+        }
+    )
+    asv = ASV(asv_config, "cpu", torch.nn.Linear(256, 256))
+    f_trials = os.path.join(eval_folder, "data", "eval_trials.txt")
+    if scenario == "ignorant":
+        f_enrolls = os.path.join(eval_folder, "data", "eval_enrolls.txt")
+    else:
+        f_enrolls = os.path.join(eval_folder, "data", "anon_eval_enrolls.txt")
+
+
+    seed_eval = args.seed_eval
+
+    # Optional: Set a seed for reproducibility
+    if seed is not None:
+        random.seed(seed)
+
+    # Step 1: Collect all unique spe
+    new_df = f_trials
+    # Collect all unique speaker_ids
+    all_speaker_ids = set()
+    with open(new_df, 'r') as file:
+        for line in file:
+            obj = json.loads(line.strip())
+            all_speaker_ids.add(obj["speaker_id"])
+
+    # #Choose min(max_speakers, len(all_speaker_ids)) random speaker_ids
+    # Generate speakers_list based on the provided start, end, and increment values
+    speakers_list = list(range(start_speakers, end_speakers + 1, increment_speakers))
+
+    for max_speakers in speakers_list:
+        with open(write_file, "a") as f:
+                    f.write(f"\n")
+        for i in range(iterations_per_group):
+            # Step 2: Choose min(max_speakers, len(all_speaker_ids)) random speaker_ids
+            chosen_ids = random.sample(list(all_speaker_ids), min(max_speakers, len(all_speaker_ids)))
+            print(f"Group size {max_speakers}, Iteration {i+1}, Chosen speakers: {chosen_ids}")
+
+            trials_ids = []
+            with open(f_trials, 'r') as trial_file:
+                for line in trial_file:
+                    obj = json.loads(line.strip())
+                    if obj["speaker_id"] in chosen_ids:
+                        trials_ids.append(obj)
+            filtered_trials = f"trials_ig{seed_eval}_{args.end}.txt"
+            with open(filtered_trials, 'w') as temp_file:
+                for entry in trials_ids:
+                    temp_file.write(json.dumps(entry) + "\n")
+                    
+            enrolls_ids = []
+            with open(f_enrolls, 'r') as enroll_file:
+                for line in enroll_file:
+                    obj = json.loads(line.strip())
+                    if obj["speaker_id"]  in chosen_ids:
+                        enrolls_ids.append(obj)
+        
+                filtered_enrolls = f"enrolls_ig{seed_eval}_{args.end}.txt"
+                with open(filtered_enrolls, 'w') as temp_file:
+                    for entry in enrolls_ids:
+                        temp_file.write(json.dumps(entry) + "\n")
+        
+                # compute SpkId vectors of all utts and map them to PLDA space
+                vecs, labels = dict(), dict()
+                for name, f in zip(["trials", "enrolls"], [filtered_trials, filtered_enrolls]):
+                    vecs[name], labels[name] = asv.compute_spkid_vecs(f)
+                    vecs[name] -= np.mean(vecs[name], axis=0)
+                    if asv.lda_model is not None:
+                        vecs[name] = asv.lda_model.transform(vecs[name])
+                    vecs[name] = asv.plda_model.model.transform(
+                        vecs[name], from_space="D", to_space="U_model"
+                    )
+                
+                # compute LLRs of all pairs of trial and enrollment utterances
+                llrs, pairs = compute_llrs(asv.plda_model, vecs, 5000)
+                del vecs
+                
+                # map utt indices to speaker indices
+                pairs[:, 0] = labels["trials"][pairs[:, 0]]
+                pairs[:, 1] = labels["enrolls"][pairs[:, 1]]
+                
+                print("Averaging LLRs across speakers")
+                print(f"No. of speaker pairs: {pairs.shape[0]}")
+                
+                # avg. LLRs across speakers and dump them to the experiment folder
+                unique_pairs, inverse = np.unique(pairs, axis=0, return_inverse=True)
+                llr_avgs = np.bincount(inverse, weights=llrs) / np.bincount(inverse)
+            
+                trials = unique_pairs[:, 0]
+                enrolls =  unique_pairs[:, 1]  
+                
+                fpr, tpr, thresholds, key = compute_eer(trials, enrolls, llr_avgs)
+                eer = (fpr[key] + (1 - tpr[key])) / 2
+                print( llr_avgs.size, thresholds[key], eer)
+                
+                with open(write_file, "a") as f:
+                    if scenario == "ignorant":
+                        f.write(f"{seed} {seed_eval } {max_speakers} {llr_avgs.size} {thresholds[key]} {eer}\n")
+                    else:
+                        f.write(f" {thresholds[key]} {eer}\n")
+
+def main(args):
+    """
+    1. Select the .txt with the experiments.
+    2. Plot the EERs of the ignorant and lazy-informed scenario.
+    """
+
+    start_speakers = args.start  # Starting number of speaker IDs
+    end_speakers = args.end  # Ending number of speaker IDs
+    increment_speakers = 50 # Increment number of speaker IDs for each step
+    iterations_per_group = 5  # Number of times you want to repeat the sampling process for each group size
+    seed = args.seed # Seed for random number generation for reproducibility
+    write_file =args.eval_file
+
+    sample_and_filter_speakers(args, write_file, start_speakers, end_speakers, increment_speakers, iterations_per_group, seed)
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--config", type=str, help="Path to eval folder")
+    parser.add_argument("--eval_file", type=str, help="Path to eval file", default= "spkanon_eval/results_stargan/ls-train-other-500/speakers_ls_train-other-500#0-lz.txt")
+    parser.add_argument("--scen", type=str, help="Which scenario", default ="ignorant")
+    parser.add_argument("--seed", type=int, help="Seed index", default=52)
+    parser.add_argument("--seed_eval", type=int, help="Seed index", default="800")
+    parser.add_argument("--start", type=int, help="Start speaker index", default=650)
+    parser.add_argument("--end", type=int, help="End speaker index", default=1050)
+    
+    args = parser.parse_args()
+    main(args)
